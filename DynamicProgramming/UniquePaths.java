02c3a951b54265a38b1ed4a294ab448c4709cab4
Thar <user@Thar.local>
2025-08-23 17:00:13 +0700

    added new

--- DynamicProgramming/UniquePaths.java
+++ DynamicProgramming/UniquePaths.java
@@ -9,6 +9,9 @@ public class UniquePaths {
         int paths=findPaths_Memoization(0, 0, grids);
         System.out.println(paths);
 
+        paths=findPaths_Tabulation(4, 3);
+        System.out.println(paths);
+
     }
 
    static int  findPaths_Memoization(int r, int c, int[][] grid){
@@ -27,6 +30,26 @@ public class UniquePaths {
         grid[r][c]=findPaths_Memoization(r+1, c, grid) + findPaths_Memoization(r, c+1, grid);
         return grid[r][c];
 
+
+    }
+
+    static int  findPaths_Tabulation(int er, int ec){
+        int[][] grid= new int[er][ec];
+        grid[er-1][ec-1]=1;
+
+        for(int r=er-1; r>=0 ; r--){
+            for(int c=ec-1; c>=0; c--){
+                if(r==er-1 && c==ec-1){
+                    continue;
+                }
+                int right_paths= c+1<ec ? grid[r][c+1] : 0;
+                int bottom_paths= r+1<er ? grid[r+1][c] : 0;
+                grid[r][c]=right_paths+bottom_paths;
+            }
+        }
+
+        return grid[0][0];
+        
 
     }
     
